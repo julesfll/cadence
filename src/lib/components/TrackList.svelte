4f0ae7bad8e4e453a16190874fec9ecc993b432e
Jules Le Menestrel <juleslemenestrel@gmail.com>
2023-01-08 19:50:25 -0500

    Add landing page copy

--- src/lib/components/TrackList.svelte
+++ src/lib/components/TrackList.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import TrackItem from './TrackItem.svelte';
 
-	export let tracks: SpotifyApi.TrackObjectFull[] | null;
+	export let tracks: SpotifyApi.TrackObjectFull[];
 </script>
 
 <ul>
 	{#if tracks}
-		{#each tracks as track}
+		{#each tracks as track (track.id)}
 			<li>
 				<TrackItem {track} />
 			</li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,31 +3,25 @@
 	import { page } from '$app/stores';
 </script>
 
-<!-- <h1>SvelteKit Auth Example</h1>
-<!-- <pre>{JSON.stringify($page, null, 2)}</pre> -->
-<!-- <p>
-	{#if $page.data.session}
-		{#if $page.data.session.user?.image}
-			<span style="background-image: url('{$page.data.session.user.image}')" />
-		{/if}
-		<span class="signedInText">
-			<small>Signed in as</small><br />
-			<strong>{$page.data.session.user?.name ?? 'User'}</strong>
-		</span>
-		<button on:click={() => signOut()} class="button">Sign out</button>
-	{:else}
-		<span class="notSignedInText">You are not signed in</span>
-		<button on:click={() => signIn('spotify')}>Sign In with Spotify</button>
-	{/if}
-</p> -->
 <main>
 	<section class="h-full bg-white p-3">
 		<h1 class="py-12 text-5xl font-semibold">Your daily jog, now with a soundtrack.</h1>
 		<button
-			on:click={() => signIn('spotify')}
+			on:click={() => signIn('spotify', { callbackUrl: '/builder' })}
 			class="border border-black py-3 px-5 font-bold hover:bg-gray-200 focus:outline-none focus:ring-8 focus:ring-gray-200"
 			>Login with Spotify</button
 		>
+	</section>
+	<section>
+		<p>Run to the beat of your favorite music without ever changing your pace.</p>
+	</section>
+	<section>
+		<h2>Spotify Integration</h2>
+		<p>
+			Access all the playlists you already have, or use Spotify's search engine to find exactly the
+			song, artist, or album you're looking for. Then export your picks as a new playlist, all
+			within the app. (can change to numbered list)
+		</p>
 	</section>
 </main>
 <pre>{JSON.stringify($page, null, 2)}</pre>
