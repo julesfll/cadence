4b36284dfa971e9d42ce8c734a801779d0523e5e
Jules Le Menestrel <juleslemenestrel@gmail.com>
2023-01-12 19:33:43 -0500

    Add custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif', 'system-ui']
+    },
     extend: {},
   },
   plugins: [],
