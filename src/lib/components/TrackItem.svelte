ef68a369017ff97b4669d4d7f1328d2634a51448
Jules Le Menestrel <juleslemenestrel@gmail.com>
2023-01-13 18:36:57 -0500

    Clean up TrackItem

--- src/lib/components/TrackItem.svelte
+++ src/lib/components/TrackItem.svelte
@@ -27,35 +27,30 @@
 	$: trackSelected = $selectedTracks.includes(track);
 </script>
 
+<!-- TODO: make more accessible -->
+<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 <div
 	on:mouseover={() => handlePlayTrack(track.preview_url)}
 	on:mouseout={() => handlePauseTrack()}
 	on:focus={() => handlePlayTrack(track.preview_url)}
 	on:blur={() => handlePauseTrack()}
+	class={`flex items-center p-2 rounded-lg focus:ring-4 focus:outline-none focus:ring-gray-200
+	${trackSelected ? 'bg-gray-300 hover:bg-gray-300' : 'hover:bg-gray-200'}`}
 	on:click={toggleTrack}
-	class={`group flex cursor-pointer items-center ${
-		trackSelected ? 'bg-gray-200 hover:bg-gray-200' : 'hover:bg-gray-100'
-	}`}
+	on:keypress={toggleTrack}
+	tabindex="0"
 >
-	<div class="flex items-center group-hover:text-blue-600">
-		<div class="h-5 w-5 m-3">
-			{#if trackSelected}
-				<Icon icon="mdi:check" class="h-5 w-5" />
-			{:else}
-				<Icon icon="mdi:plus" class="h-5 w-5" />
-			{/if}
+	{#if 'album' in track && showAlbum}
+		<div class="shrink-0">
+			<img
+				class="h-20 w-20 object-cover"
+				src={track.album.images[track.album.images.length - 1].url}
+				alt={track.album.name}
+			/>
 		</div>
-		<span class="sr-only">Add track</span>
-	</div>
-	{#if track.album && showAlbum}
-		<img
-			class="h-20 w-20 object-cover"
-			src={track.album.images[track.album.images.length - 1].url}
-			alt={track.album.name}
-		/>
 	{/if}
-	<div class="space-y-1 px-3 py-2">
-		<h3 class="text-ellipsis">{track.name}</h3>
+	<div class="flex-1 flex flex-col items-start justify-between ml-2 h-20">
+		<h3 class="truncate">{track.name}</h3>
 		<p class="text-sm text-gray-500">
 			{#if track.explicit}
 				<span class="mr-1 bg-gray-200 px-1.5 py-0.5 text-xs font-medium">E</span>
@@ -66,4 +61,13 @@
 			<Badge icon="mdi:metronome-tick">{Math.round(track.tempo)} BPM</Badge>
 		{/if}
 	</div>
+	{#if trackSelected}
+		<div class="p-3 bg-gray-200 rounded-full">
+			<Icon icon="mdi:check" class="h-5 w-5 " />
+		</div>
+	{:else}
+		<div class="p-3">
+			<Icon icon="mdi:plus" class="h-5 w-5" />
+		</div>
+	{/if}
 </div>
