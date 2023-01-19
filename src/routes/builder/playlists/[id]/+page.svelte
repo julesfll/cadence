41e6fb5b4904c1a1f4c2079c394244fb35116f8d
Jules Le Menestrel <juleslemenestrel@gmail.com>
2023-01-19 18:21:04 -0500

    Fix playlist with no image bug

--- src/routes/builder/playlists/[id]/+page.svelte
+++ src/routes/builder/playlists/[id]/+page.svelte
@@ -10,24 +10,26 @@
 	const { playlist, tracksWithTempos } = data;
 </script>
 
-<!-- 
-	public/private? {playlist.public}
-{playlist.name}
-description {playlist.description}
-{playlist.owner.display_name}, {playlist.followers.total} likes, playlist.tracks.items.length, total time
-open in spotify
- -->
 <Container>
 	<div class="flex flex-wrap gap-4 mb-2">
-		<img
-			src={playlist.images[0].url}
-			alt={playlist.name}
-			class="shadow-xl h-48 w-48 object-cover"
-		/>
+		{#if playlist.images.length}
+			<img
+				class="shadow-xl h-48 w-48 object-cover"
+				src={playlist.images[0].url}
+				alt={playlist.name}
+			/>
+		{:else}
+			<div class="bg-gray-700 grid place-items-center shadow-xl h-48 w-48  object-cover">
+				<Icon icon="mdi:playlist-music" class="h-1/2 w-1/2 text-slate-50" />
+			</div>
+		{/if}
+
 		<div class="basis-3/5 flex-grow">
-			<p class="text-xs tracking-wide text-gray-500">
-				{playlist.public ? 'PUBLIC' : 'PRIVATE'} PLAYLIST
-			</p>
+			{#if playlist.public !== null}
+				<p class="text-xs tracking-wide text-gray-500">
+					{playlist.public ? 'PUBLIC' : 'PRIVATE'} PLAYLIST
+				</p>
+			{/if}
 			<h1 class="text-3xl my-3 font-extrabold leading-none tracking-tight text-gray-900">
 				{playlist.name}
 			</h1>
@@ -36,8 +38,11 @@ open in spotify
 				{playlist.owner.display_name} &middot {formatNumber(playlist.followers.total)} likes &middot
 				{playlist.tracks.items.length} songs
 			</p>
-			<a href="spotify:playlist:{playlist.id}" class="text-gray-800 hover:text-gray-600 text-sm mb-2 mt-3 flex items-center"
-					><Icon class="inline mr-1 h-5 w-5" icon="mdi:spotify" />Open in Spotify</a>
+			<a
+				href="spotify:playlist:{playlist.id}"
+				class="text-gray-800 hover:text-gray-600 text-sm mb-2 mt-3 flex items-center"
+				><Icon class="inline mr-1 h-5 w-5" icon="mdi:spotify" />Open in Spotify</a
+			>
 		</div>
 	</div>
 	<TrackList tracks={tracksWithTempos} />
